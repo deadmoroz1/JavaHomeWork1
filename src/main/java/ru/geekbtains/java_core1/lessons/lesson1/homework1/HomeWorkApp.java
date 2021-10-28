77bbefe737fceb4d3e34ec55328da638fbd745c4
Anatoly Zabelin <anatolyzabelin@Anatolys-MacBook-Pro.local>
2021-10-28 18:44:14 +0300

    Initial commit

---
+++ src/main/java/ru/geekbtains/java_core1/lessons/lesson1/homework1/HomeWorkApp.java
@@ -0,0 +1,47 @@
+package ru.geekbtains.java_core1.lessons.lesson1.homework1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 20;
+        int b = 30;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = -100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value >0 && value <=100) {
+            System.out.println("Желтый");
+        } else if (value >100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 200;
+        int b = 600;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else System.out.println("a < b");
+    }
+}
